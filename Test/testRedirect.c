ff8b442e02de2e4e90ef540a19886df7322a3b84
prachi <prachi10k89@gmail.com>
2020-10-21 16:19:01 +0530

    Code cleanup.

--- Test/testRedirect.c
+++ Test/testRedirect.c
@@ -1,5 +1,6 @@
 /*
-This is to test redirect with following inputs:
+This test has to be run on top of linux machine.
+This is to test reading arbitrary data and writing it to a file with following inputs:
 	1. string
     2. binary structure
 	3. file
@@ -31,7 +32,7 @@ FILE* run() {
     return popenFp;
 }
 
-void cleanup( FILE* popenFp, int outFd, FILE* outFp ) {
+void cleanup( FILE* popenFp, int outFd, FILE* outFp, char* output ) {
     if ( popenFp )
 	    pclose( popenFp );
     if ( outFd )
@@ -39,62 +40,79 @@ void cleanup( FILE* popenFp, int outFd, FILE* outFp ) {
     if ( outFp )
         fclose( outFp );
 
+    if ( output )
+        free( output );
+
     popenFp = NULL;
     outFd = -1;
     outFp = NULL;
+    output = NULL;
 
     printf("-----------------------------------------------------------------\n");
 }
 
-int main( void ) {
-
+char* runTest( const char* input, int size, int count ) {
+    
     reset();
-    /******************************  Test Case 1 - Input String ******************************/
-    printf("Test Case 1: String ...\n");
-	FILE* popenFp = run();
 
-    // give input to exe
-    char input[] = "Hello World! Redirect: test newline \n test tab \t test carriage return \r\n END!";
-	int ret = write( fileno( popenFp ), input, sizeof(input) );
-	if ( ret < sizeof(input) ) {
-		fprintf(stderr, "Error: write(), return status %d(%s)\n", errno, strerror(errno) );
+    FILE* popenFp = run();
+
+    int ret = fwrite( input, size, count, popenFp );
+	if ( ret < 2 ) {
+		fprintf(stderr, "Error: fwrite(), return status %d(%s)\n", errno, strerror(errno) );
         pclose( popenFp );
-		return -1;
+		return NULL;
 	}
-
+    fflush( popenFp );
     sleep(1);
-    int outFd = open("/tmp/redirectoutfile", O_RDONLY, 0644 );
-    if ( outFd < 0 ) {
+
+    FILE* outFp = fopen("/tmp/redirectoutfile", "r" );
+    if ( !outFp ) {
         fprintf(stderr, "Failed to open file '/tmp/redirectoutfile', return status: %d(%s)\n", errno, strerror(errno));
         pclose( popenFp );
-        return -1;
+        return NULL;
     }
     
-    char output[sizeof(input)] = {0};
-    ret = read( outFd, output, sizeof(input) );
-	if ( ret < sizeof(input) ) {
-		fprintf(stderr, "Error: read(), return status %d(%s)\n", errno, strerror(errno) );
-        pclose( popenFp );
-        close(outFd);
-		return -1;
+    char *output = (char*)malloc(size*count);
+    if ( !output ) {
+        fprintf(stderr, "Error: malloc failed, %d(%s)\n", errno, strerror(errno));
+        exit(-1);
+    }
+
+    // struct empInfo empOut[2];
+    ret = fread( output, size, count, outFp );
+	if ( ret < 2 ) {
+		fprintf(stderr, "Error: fread(), return status %d(%s)\n", errno, strerror(errno) );
+        cleanup( popenFp, 0, outFp, NULL );
+		return NULL;
 	}
 
-    // compare text input and output
-    printf("Given input: [%s], \nOutput: [%s]\n", input, output);
-    if ( strcmp( input, output ) == 0 ) {
-        printf("Test Case 1: String - PASSED\n");
-    } else {
-        printf("Test Case 1: String - FAILED\n");
-    }
-    cleanup( popenFp, outFd, NULL );
+    cleanup( popenFp, 0, outFp, NULL );
+    return output;
+}
 
+int main( void ) {
+
+    /******************************  Test Case 1 - Input String ******************************/
+    printf("Test Case 1: String ...\n");
+    char input[] = "Hello World! Redirect: test newline \n test tab \t test carriage return \r\n END!";
+    char* output = runTest( input, 1, sizeof(input) );
+    if ( !output ) {
+        fprintf(stderr, "Error: running test 1\n");
+    } else {
+        // compare text input and output
+        printf("Given input: [%s], \nOutput: [%s]\n", input, output);
+        if ( strcmp( input, output ) == 0 ) {
+            printf("Test Case 1: String - PASSED\n");
+        } else {
+            printf("Test Case 1: String - FAILED\n");
+        }
+        cleanup( NULL, 0, NULL, output );
+    }
 
 
     /******************************  Test Case 2 - Binary Structure Input ******************************/
     printf("Test Case 2: Binary Structure ...\n");
-    reset();
-	popenFp = run();
-
     // give input to redirect exe
     struct empInfo {
         char name[20];
@@ -105,59 +123,39 @@ int main( void ) {
     strcpy(emp[0].name, "John"); strcpy(emp[0].location, "Europe"); emp[0].id = 1001;
     strcpy(emp[1].name, "Marry"); strcpy(emp[1].location, "India"); emp[1].id = 2001;
 
-	ret = fwrite( emp, sizeof(struct empInfo), 2, popenFp );
-	if ( ret < 2 ) {
-		fprintf(stderr, "Error: fwrite(), return status %d(%s)\n", errno, strerror(errno) );
-        pclose( popenFp );
-		return -1;
-	}
-    fflush( popenFp );
-    sleep(1);
-
-    FILE* outFp = fopen("/tmp/redirectoutfile", "r" );
-    if ( !outFp ) {
-        fprintf(stderr, "Failed to open file '/tmp/redirectoutfile', return status: %d(%s)\n", errno, strerror(errno));
-        pclose( popenFp );
-        return -1;
-    }
-    
-    struct empInfo empOut[2];
-    ret = fread( empOut, sizeof(struct empInfo), 2, outFp );
-	if ( ret < 2 ) {
-		fprintf(stderr, "Error: fread(), return status %d(%s)\n", errno, strerror(errno) );
-        pclose( popenFp );
-        fclose( outFp );
-		return -1;
-	}
-
-    // compare binary structure input and output
-    printf("Given input: \n");
-    for (int i = 0; i < 2; i++ )
-        printf("\tname [%s] location [%s] id [%d]\n", emp[i].name, emp[i].location, emp[i].id );
-
-    printf("Output: \n");
-    for (int i = 0; i < 2; i++ )
-        printf("\tname [%s] location [%s] id [%d]\n", empOut[i].name, empOut[i].location, empOut[i].id );
-
-    int n = memcmp( &emp, &empOut, 2*sizeof(struct empInfo) );
-    if ( n == 0 ) {
-        printf("Test Case 2: Binary Structure - PASSED\n");
+    struct empInfo *empOut = ( struct empInfo* ) runTest( (char*)emp, sizeof(struct empInfo), 2 );
+    if ( !empOut ) {
+        fprintf(stderr, "Error: running test 2\n");
     } else {
-        printf("Test Case 2: Binary Structure - FAILED\n");
+        // compare binary structure input and output
+        printf("Given input: \n");
+        for (int i = 0; i < 2; i++ )
+            printf("\tname [%s] location [%s] id [%d]\n", emp[i].name, emp[i].location, emp[i].id );
+
+        printf("Output: \n");
+        for (int i = 0; i < 2; i++ )
+            printf("\tname [%s] location [%s] id [%d]\n", empOut[i].name, empOut[i].location, empOut[i].id );
+
+        int n = memcmp( emp, empOut, 2*sizeof(struct empInfo) );
+        if ( n == 0 ) {
+            printf("Test Case 2: Binary Structure - PASSED\n");
+        } else {
+            printf("Test Case 2: Binary Structure - FAILED\n");
+        }
+        cleanup( NULL, 0, NULL, (char*)empOut );
     }
-    cleanup( popenFp, 0, outFp );
 
 
     /******************************  Test Case 3 - File Input ******************************/
     printf("Test Case 3: File ...\n");
     reset();
-    ret = system( "cat ./InputImage.jpeg | ../redirect" );
+    int ret = system( "cat ./InputImage.jpeg | ../redirect" );
 	if ( ret < 0 ) {
 		fprintf(stderr, "Error: system('cat ./InputImage.jpeg | ../redirect') , return status %d(%s)\n", errno, strerror(errno) );
 		exit(-1);
 	}
 
-    popenFp = popen( "diff ./InputImage.jpeg /tmp/redirectoutfile", "r" );
+    FILE* popenFp = popen( "diff ./InputImage.jpeg /tmp/redirectoutfile", "r" );
     if ( !popenFp ) {
         fprintf(stderr, "Error: popen('diff ./InputImage.jpeg /tmp/redirectoutfile'), return status %d(%s) \n", errno, strerror(errno));
         exit(-1);
@@ -172,6 +170,6 @@ int main( void ) {
         printf("Test Case 3: File 'Not Matched' - FAILED\n");
     }
 
-    cleanup( popenFp, 0, NULL );
+    cleanup( popenFp, 0, NULL, NULL );
 	return 0;
 }
